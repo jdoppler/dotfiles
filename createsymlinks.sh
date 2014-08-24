a6727f17b374389eeeb753fb0bcbb1676f7d08db
jdoppler <joerg.doppler@tuwien.ac.at>
2014-08-24 10:37:27 +0200

    added check for existing symlinks

--- createsymlinks.sh
+++ createsymlinks.sh
@@ -2,15 +2,28 @@
 ###############################################################################
 #   
 #   createsymlinks.sh
-#   create symlinks from $HOME to all files/folders in ~/.dotfiles
-#   adapted from https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+#    create symlinks from $HOME to all files/folders in ~/.dotfiles
+#    adapted from https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+#    and https://github.com/mathiasbynens/dotfiles/blob/master/bootstrap.sh
 #
 
 DOTDIR=$HOME/.dotfiles
 FILES="bashrc inputrc vim vimrc"
 
 for file in $FILES; do
+    DOTFILE=$HOME/.$file
+
     echo "Creating symlink to $file in \$HOME..."
-    ln -s $DOTDIR/$file $HOME/.$file
+
+    if [[ -L "$DOTFILE" ]]; then
+        echo "Warning: $DOTFILE symlink to $(readlink $DOTFILE) already exists"
+        read -p "Overwrite? (y/n) " -n 1
+
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            ln -sf $DOTDIR/$file $DOTFILE
+        fi;
+    else
+        ln -s $DOTDIR/$file $DOTFILE
+    fi
     echo "...done"
 done
