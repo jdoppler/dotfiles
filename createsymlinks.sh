9bca12cd82327d4ce3bcad6ccde30f57adadb86e
jdoppler <joerg.doppler@tuwien.ac.at>
2015-05-21 15:50:07 +0200

    bugfix: when replacing already symlinked folder, a symlink is created in the directory it points to

--- createsymlinks.sh
+++ createsymlinks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ###############################################################################
-#   
+#
 #   createsymlinks.sh
 #    create symlinks from $HOME to all files/folders in ~/.dotfiles
 #    adapted from https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
 #    and https://github.com/mathiasbynens/dotfiles/blob/master/bootstrap.sh
+#    and http://blog.endpoint.com/2009/09/using-ln-sf-to-replace-symlink-to.html
+#     (to fix a bug when replacing a symlinked folder)
 #
 
 DOTDIR=$HOME/.dotfiles
@@ -29,7 +31,7 @@ for file in $FILES; do
         read -p "Overwrite? (y/n) " -n 1
 
         if [[ $REPLY =~ ^[Yy]$ ]]; then
-            ln -sf $DOTDIR/$file $DOTFILE
+            ln -sfn $DOTDIR/$file $DOTFILE
         fi
     else
         ln -s $DOTDIR/$file $DOTFILE
